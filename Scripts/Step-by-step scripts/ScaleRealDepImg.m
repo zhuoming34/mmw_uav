8fdefe730db4e2c0b73f1376b9712297f151c632
zhuoming34 <62568509+zhuoming34@users.noreply.github.com>
2021-06-01 13:39:40 -0400

    Update ScaleRealDepImg.m

--- Scripts/Step-by-step scripts/ScaleRealDepImg.m
+++ Scripts/Step-by-step scripts/ScaleRealDepImg.m
@@ -25,8 +25,8 @@ sn_pz = -sn_z./sn_ratio;
 
 N_col_l = ceil(abs((sn_px(3)-xl))/(pxSize/1000));
 N_col_r = ceil(abs((sn_px(1)-xr))/(pxSize/1000));
-N_row_t = ceil(abs((sn_pz(1)-zt))/(pxSize/1000));
-N_row_b = ceil(abs((sn_pz(2)-zb))/(pxSize/1000));
+N_row_b = ceil(abs((sn_pz(1)-zt))/(pxSize/1000));
+N_row_t = ceil(abs((sn_pz(2)-zb))/(pxSize/1000));
 
 map1 = jet; map2 = gray;
 
@@ -133,7 +133,7 @@ if show_photo == 1
     grid off;
 end
 
-function [Ib,N_row,N_col] = depScale(I,sn_px,xl,xr,zt,zb)        
+function [It,N_row,N_col] = depScale(I,sn_px,xl,xr,zt,zb)        
     if sn_px(3) <= xl || sn_px(1) >= xr
         col_l = ones(numr,N_col_l); col_r = ones(numr,N_col_r); % extend
         Ic = [col_l,I,col_r];
@@ -143,21 +143,21 @@ function [Ib,N_row,N_col] = depScale(I,sn_px,xl,xr,zt,zb)
         N_col = numc - N_col_l - N_col_r;
     end
 
-    if sn_pz(1) >= zt
-        row_t = ones(N_row_t,N_col); % extend
-        It = [row_t;Ic];
-        N_row = numr + N_row_t;
-    else
-        It = Ic(N_row_t+1:end,:); % crop
-        N_row = numr - N_row_t;
-    end
-
-    if sn_pz(2) <= zb
+    if sn_pz(1) >= zt % positive on projection plane
         row_b = ones(N_row_b,N_col); % extend
         Ib = [Ic;row_b];
-        N_row = N_row + N_row_b;
+        N_row = numr + N_row_b;
     else
-        Ib = It(1:end-N_row_b,:); % crop
-        N_row = N_row - N_row_b;
+        Ib = Ic(1:end-N_row_b,:); % crop
+        N_row = numr - N_row_b;
+    end
+
+    if sn_pz(2) <= zb % negative on projection plane
+        row_t = ones(N_row_t,N_col); % extend
+        It = [row_t;Ic];
+        N_row = N_row + N_row_t;
+    else
+        It = Ib(N_row_t+1:end,:); % crop
+        N_row = N_row - N_row_t;
     end
 end
