8c915ddb03606ab2c011934ccb8a26924068941a
zhuoming34 <62568509+zhuoming34@users.noreply.github.com>
2021-06-03 21:30:59 -0400

    Update heatSph2Cart.m

--- Scripts/Sample Synthesizer/functions/heatSph2Cart.m
+++ Scripts/Sample Synthesizer/functions/heatSph2Cart.m
@@ -1,13 +1,14 @@
-function heatmap_ct = heatSph2Cart(heatmap, ct_coord, scene_lim, N_x, N_y, N_z)
+function heatmap_ct = heatSph2Cart(heatmap, ct_coord, scene_lim, N_x, N_y, N_z, ptGrid, ptGrid_heat)
         
         addpath('sph2cart_functions');
 
-        % match intensity values to corresponding points
+        % match intensity values to corresponding spherical voxel center
         radar_heat = matchHeat(heatmap,N_phi,N_rho,N_theta);
 
-        % to reduce the size of heatmap
-        % gets points whose intensities are larger than the threshold
-        threshold = max(max(max(heatmap)))/10;
+        %{
+        % ver 2.3
+        % get points whose intensities are larger than the threshold
+        threshold = 0;%max(max(max(heatmap)))/50;
         idx_heat_fl = find(radar_heat >= threshold);
         pt_heat_fl = zeros(size(idx_heat_fl,1),3);
         heat_fl = zeros(size(idx_heat_fl,1),1);
@@ -15,14 +16,27 @@ function heatmap_ct = heatSph2Cart(heatmap, ct_coord, scene_lim, N_x, N_y, N_z)
             pt_heat_fl(i,:) = ct_coord(idx_heat_fl(i),:);
             heat_fl(i) = radar_heat(idx_heat_fl(i));
         end
-
         % no filtering
         %pt_heat_fl = ct_coord;
         %heat_fl = radar_heat;
         %x_hf = pt_heat_fl(:,1); y_hf = pt_heat_fl(:,2); z_hf = pt_heat_fl(:,3);
-        
         % convert heatmap from spherical to cartesian coordinate system
-        %N_x= 64; N_y = 256; N_z = 64;
-        %scene_lim = [-4, 4; 3, 10; -1.25, 2.75];
         heatmap_ct = sph2cart_heat(scene_lim,N_x,N_y,N_z,pt_heat_fl,heat_fl);
+        %}
+        
+        % ver 3.1
+        ptGrid_clss = knnsearch(ct_coord,ptGrid,'K',1); % classification
+        for i = 1:numPoint
+            ptGrid_heat(i) = radar_heat(ptGrid_clss(i));
+        end 
+        heatmap_ct = sph2cart_heat2(scene_lim,N_x,N_y,N_z,ptGrid,ptGrid_heat);
+        
+        % logarithmic scale
+        heatmap_ct = heatmap_ct + 1.0;
+        heatmap_ct = log(heatmap_ct);
+        
+        % ver 3.3 filtering
+        maxheat = max(max(max(heatmap_ct)));
+        filter_idx = find(heatmap_ct < maxheat*0.675);
+        heatmap_ct(filter_idx) = 0;        
 end
