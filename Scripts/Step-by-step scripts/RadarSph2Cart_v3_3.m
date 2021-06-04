e4480d8d92b96eb91763fac89175e39608ae0a9f
zhuoming34 <62568509+zhuoming34@users.noreply.github.com>
2021-06-04 17:53:14 -0400

    Update RadarSph2Cart_v3_3.m

--- Scripts/Step-by-step scripts/RadarSph2Cart_v3_3.m
+++ Scripts/Step-by-step scripts/RadarSph2Cart_v3_3.m
@@ -32,29 +32,34 @@ for idx_mat = 1:500
         % match intensity values to corresponding spherical voxel center
         radar_heat = matchHeat(heatmap,N_phi,N_rho,N_theta);
 
-        %{
         % ver 2.3
         % get points whose intensities are larger than the threshold
-        threshold = 0;%max(max(max(heatmap)))/50;
-        idx_heat_fl = find(radar_heat >= threshold);
-        pt_heat_fl = zeros(size(idx_heat_fl,1),3);
-        heat_fl = zeros(size(idx_heat_fl,1),1);
-        for i = 1:size(idx_heat_fl,1)
-            pt_heat_fl(i,:) = ct_coord(idx_heat_fl(i),:);
-            heat_fl(i) = radar_heat(idx_heat_fl(i));
+        threshold = max(max(max(heatmap)))/50;
+        idx_heat_fliter = find(radar_heat >= threshold);
+        ct_coord_filtered = zeros(size(idx_heat_fliter,1),3);
+        radar_heat_filtered = zeros(size(idx_heat_fliter,1),1);
+        for i = 1:size(idx_heat_fliter,1)
+            ct_coord_filtered(i,:) = ct_coord(idx_heat_fliter(i),:);
+            radar_heat_filtered(i) = radar_heat(idx_heat_fliter(i));
         end
+        % no filtering
+        %pt_heat_fl = ct_coord;
+        %heat_fl = radar_heat;
         %x_hf = pt_heat_fl(:,1); y_hf = pt_heat_fl(:,2); z_hf = pt_heat_fl(:,3);
         % convert heatmap from spherical to cartesian coordinate system
-        heatmap_ct = sph2cart_heat(scene_lim,N_x,N_y,N_z,pt_heat_fl,heat_fl);
-        %}
+        % heatmap_ct = sph2cart_heat(scene_lim,N_x,N_y,N_z,pt_heat_fl,heat_fl);
+        
         
         % ver 3.1
-        ptGrid_clss = knnsearch(ct_coord,ptGrid,'K',1); % classification
+        %ptGrid_clss = knnsearch(ct_coord,ptGrid,'K',1); % classification
+        ptGrid_clss = knnsearch(ct_coord_filtered,ptGrid,'K',1); % classification
         for i = 1:numPoint
-            ptGrid_heat(i) = radar_heat(ptGrid_clss(i));
+            %ptGrid_heat(i) = radar_heat(ptGrid_clss(i));
+            ptGrid_heat(i) = radar_heat_filtered(ptGrid_clss(i));
         end 
         heatmap_ct = sph2cart_heat2(scene_lim,N_x,N_y,N_z,ptGrid,ptGrid_heat);
         
+        %{
         % logarithmic scale
         heatmap_ct = heatmap_ct + 1.0;
         heatmap_ct = log(heatmap_ct);
@@ -62,7 +67,8 @@ for idx_mat = 1:500
         % ver 3.3 filtering
         maxheat = max(max(max(heatmap_ct)));
         filter_idx = find(heatmap_ct < maxheat*0.675);
-        heatmap_ct(filter_idx) = 0;
+        heatmap_ct(filter_idx) = 0;  
+        %}
         
         save(strcat(saveaddr,'md_',num2str(CAD_idx),'_pm_',num2str(idx_mat+offset),'_cam_',num2str(cam),'_cart_heatmap_noisy','.mat'), 'heatmap_ct');
         disp(strcat("Model ", num2str(CAD_idx),", placement ", num2str(idx_mat+offset),', camera ',num2str(cam), ": done"));
