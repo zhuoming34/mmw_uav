92c7311a94ed942e988e71fe3c1659a70fcec4da
zhuoming34 <62568509+zhuoming34@users.noreply.github.com>
2021-06-06 22:09:52 -0400

    Update heatSph2Cart.m

--- Scripts/Sample Synthesizer/functions/heatSph2Cart.m
+++ Scripts/Sample Synthesizer/functions/heatSph2Cart.m
@@ -1,25 +1,30 @@
-function heatmap_ct = heatSph2Cart(heatmap, scene_lim, N_x, N_y, N_z, ptGrid, ptGrid_heat, ptGrid_clss)
-        
+%function heatmap_ct = heatSph2Cart(heatmap, scene_lim, N_x, N_y, N_z, ptGrid, ptGrid_heat, ptGrid_clss)
+function heatmap_ct = heatSph2Cart(heatmap, scene_lim, N_x, N_y, N_z, ct_coord)        
     addpath('sph2cart_functions');
 
     % match intensity values to corresponding spherical voxel center
     radar_heat = matchHeat(heatmap,N_phi,N_rho,N_theta);
-    for i = 1:numPoint
-        ptGrid_heat(i) = radar_heat(ptGrid_clss(i));
-    end 
+    %for i = 1:length(ptGrid_clss)
+    %    ptGrid_heat(i) = radar_heat(ptGrid_clss(i));
+    %end 
 
     % filtering: get points whose intensities are larger than the threshold
     threshold = max(max(max(heatmap)))/50;
-    idx_heat_fliter = find(ptGrid_heat >= threshold);
-    ptGrid_filtered = zeros(size(idx_heat_fliter,1),3);
-    ptGrid_heat_filtered = zeros(size(idx_heat_fliter,1),1);
+    %idx_heat_fliter = find(ptGrid_heat >= threshold);
+    idx_heat_fliter = find(radar_heat >= threshold);
+    points_selected = zeros(size(idx_heat_fliter,1),3);
+    heat_selected = zeros(size(idx_heat_fliter,1),1);
     for i = 1:size(idx_heat_fliter,1)
-        ptGrid_filtered(i,:) = ptGrid(idx_heat_fliter(i),:);
-        ptGrid_heat_filtered(i) = ptGrid_heat(idx_heat_fliter(i));
+        points_selected(i,:) = ct_coord(idx_heat_fliter(i),:);
+        heat_selected(i) = radar_heat(idx_heat_fliter(i));
+        %points_selected(i,:) = ptGrid(idx_heat_fliter(i),:);
+        %heat_selected(i) = ptGrid_heat(idx_heat_fliter(i));
     end
 
     % generate 3d heatmap in Cartesian
-    heatmap_ct = sph2cart_heat2(scene_lim,N_x,N_y,N_z,ptGrid_filtered,ptGrid_heat_filtered);
+    heatmap_ct = sph2cart_heat(scene_lim,N_x,N_y,N_z,points_selected,heat_selected);
+    %heatmap_ct = sph2cart_heat2(scene_lim,N_x,N_y,N_z,points_selected,heat_selected);
+    %heatmap_ct = sph2cart_heat2(scene_lim,N_x,N_y,N_z,ptGrid,ptGrid_heat);
 
     %{
     % logarithmic scale
